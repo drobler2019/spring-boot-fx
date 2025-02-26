3519b6cde16dad1478fb5fed6fcd658c87b4f77e
drobler2019 <drobler2019@gmail.com>
2025-02-26 00:31:28 -0500

    Agregado imagen en el panel

--- src/main/java/com/jxf/jasypt/config/JasyptInitializr.java
+++ src/main/java/com/jxf/jasypt/config/JasyptInitializr.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
@@ -17,6 +18,8 @@ public class JasyptInitializr implements ApplicationListener<JasyptEvent> {
 
     @Value("classpath:/index.fxml")
     private Resource resource;
+    @Value("classpath:/icon/spring.png")
+    private Resource image;
     private final String applicationTitle;
     private final ApplicationContext applicationContext;
 
@@ -29,12 +32,17 @@ public class JasyptInitializr implements ApplicationListener<JasyptEvent> {
     @Override
     public void onApplicationEvent(JasyptEvent event) {
         Platform.runLater(() -> {
+            Image img;
             try {
+                try (var stream = image.getInputStream()) {
+                    img = new Image(stream);
+                }
                 var url = resource.getURL();
                 var loader = new FXMLLoader(url);
                 loader.setControllerFactory(applicationContext::getBean);
                 Parent root = loader.load();
                 var stage = event.getStage();
+                stage.getIcons().add(img);
                 stage.setTitle(applicationTitle);
                 var scene = new Scene(root);
                 stage.setScene(scene);
